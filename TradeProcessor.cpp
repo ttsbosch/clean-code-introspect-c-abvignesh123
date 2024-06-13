abcb927906a185511ddf9941a15caf1f75604197
abvignesh123 <abvignesh88@gmail.com>
2024-06-13 16:22:40 +0530

    Update TradeProcessor.cpp
    Moved convertion part to hexa file.

--- TradeProcessor.cpp
+++ TradeProcessor.cpp
@@ -4,27 +4,9 @@
 #include <errno.h>
 #include <TradeRecord.h>
 #include <SplitString.h>
-#include <Converter.h>
+#include <Conversion.h>
 
 
-
-int GetIntegerFromString(const char* str, int* value) {
-    char* endptr;
-    *value = strtol(str, &endptr, 10);
-    if (endptr == str) {
-        return True;
-    }
-    return False;
-}
-
-int toDouble(const char* str, double* value) {
-    char* endptr;
-    *value = strtod(str, &endptr);
-    if (endptr == str) {
-        return True;
-    }
-    return False;
-}
 
 void Process(FILE* stream) {
     char line[1024];
