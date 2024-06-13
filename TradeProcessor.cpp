35d7dad96c3dd4df1cb0e58913125ce8781421b0
abvignesh123 <abvignesh88@gmail.com>
2024-06-13 16:19:04 +0530

    Update TradeProcessor.cpp
    Moving Split String to hexa file.

--- TradeProcessor.cpp
+++ TradeProcessor.cpp
@@ -2,57 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <TradeRecord.h>
+#include <SplitString.h>
+#include <Converter.h>
 
-typedef struct {
-    char SC[256]; // SourceCurrency
-    char DC[256]; // DestibationCurrency
-    float Lots;
-    double Price;
-} TradeRecord;
-
-
-char** SplitString(const char* str, char delimiter) {
-    int count = 0;
-    const char* strPtr = str;
-    
-    // Count the number of delimiters in the string
-    while (*strPtr != '\0') {
-        if (*strPtr++ == delimiter) {
-            count++;
-        }
-    }
-
-    // Allocate memory for the tokens array
-    char** tokens = (char**)malloc(sizeof(char*) * (count + 2));
-    int tokenIndex = 0;
-    strPtr = str;
-    
-    // Allocate memory for the current token
-    char* token = (char*)malloc(strlen(str) + 1);
-    int tokenLength = 0;
-    
-    // Split the string into tokens
-    while (*strPtr != '\0') {
-        if (*strPtr == delimiter) {
-            token[tokenLength] = '\0';
-            tokens[tokenIndex++] = strdup(token);
-            tokenLength = 0;
-        } else {
-            token[tokenLength++] = *strPtr;
-        }
-        strPtr++;
-    }
-    
-    // Add the last token to the tokens array
-    token[tokenLength] = '\0';
-    tokens[tokenIndex++] = strdup(token);
-    tokens[tokenIndex] = NULL;
-    
-    // Free the memory allocated for the current token
-    free(token);
-    
-    return tokens;
-}
 
 
 int GetIntegerFromString(const char* str, int* value) {
