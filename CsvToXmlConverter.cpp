5ecc92c2cb54577bd54af050a2632f8a84b352be
abvignesh123 <abvignesh88@gmail.com>
2024-06-27 16:58:57 +0530

    Update CsvToXmlConverter.cpp

--- CsvToXmlConverter.cpp
+++ CsvToXmlConverter.cpp
@@ -2,66 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
-#include <TradeRecord.h>
-#include <SplitString.h>
-#include <Converter.h>
-
-
-
-void CSVtoXmlConverter(FILE* stream) {
-    char line[1024];
-    TradeRecord objects[1024];
-    int lineCount = 0;
-    int objectCount = 0;
-
-    while (fgets(line, sizeof(line), stream)) {
-        char* fields[3];
-        int fieldCount = 0;
-        char* token = strtok(line, ",");
-        while (token != NULL) {
-            fields[fieldCount++] = token;
-            token = strtok(NULL, ",");
-        }
-
-        if (fieldCount != 3) {
-            fprintf(stderr, "WARN: Line %d malformed. Only %d field(s) found.\n", lineCount + 1, fieldCount);
-            continue;
-        }
-
-        if (strlen(fields[0]) != 6) {
-            fprintf(stderr, "WARN: Trade currencies on line %d malformed: '%s'\n", lineCount + 1, fields[0]);
-            continue;
-        }
-
-        int tradeAmount;
-        if (!GetIntegerFromString(fields[1], &tradeAmount)) {
-            fprintf(stderr, "WARN: Trade amount on line %d not a valid integer: '%s'\n", lineCount + 1, fields[1]);
-        }
-
-        double tradePrice;
-        if (!toDouble(fields[2], &tradePrice)) {
-            fprintf(stderr, "WARN: Trade price on line %d not a valid decimal: '%s'\n", lineCount + 1, fields[2]);
-        }
-
-        strncpy(objects[objectCount].SC, fields[0], 3);
-        strncpy(objects[objectCount].DC, fields[0] + 3, 3);
-        objects[objectCount].Lots = tradeAmount / LotSize;
-        objects[objectCount].Price = tradePrice;
-        objectCount++;
-        lineCount++;
+#include "CSVtoXMLConverter.h"
+ 
+int main() {
+    FILE* file = fopen("../trades.txt", "r");
+    if (file == NULL) {
+        fprintf(stderr, "ERROR: Could not open file.\n");
+        return 1;
     }
-
-    FILE* outFile = fopen("output.xml", "w");
-    fprintf(outFile, "<TradeRecords>\n");
-    for (int index = 0; index < objectCount; index++) {
-        fprintf(outFile, "\t<TradeRecord>\n");
-        fprintf(outFile, "\t\t<SourceCurrency>%s</SourceCurrency>\n", objects[index].SC);
-        fprintf(outFile, "\t\t<DestinationCurrency>%s</DestinationCurrency>\n", objects[index].DC);
-        fprintf(outFile, "\t\t<Lots>%d</Lots>\n", objects[index].Lots);
-        fprintf(outFile, "\t\t<Price>%f</Price>\n", objects[index].Price);
-        fprintf(outFile, "\t</TradeRecord>\n");
-    }
-    fprintf(outFile, "</TradeRecords>");
-    fclose(outFile);
-    printf("INFO: %d trades processed\n", objectCount);
+    ConvertFromCSVToXML(file);
+    fclose(file);
+    return 0;
 }
